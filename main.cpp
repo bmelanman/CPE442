f0f6084f1749e7fed3d675c494ae57a3c6a6cebe
Bryce Melander <brycemelander@gmail.com>
2022-09-22 13:48:59 -0700

    updated

--- main.cpp
+++ main.cpp
@@ -1,22 +1,44 @@
-//Include Libraries
-#include <opencv2/opencv.hpp>
-#include<iostream>
- 
-// Namespace nullifies the use of cv::function();
-using namespace std;
-using namespace cv;
- 
-// Read an image
-Mat img_grayscale = imread("test.jpg", 0);
+// Include Libraries
+#include "opencv2/opencv.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
 
-// Display the image.
-imshow("grayscale image", img_grayscale);
- 
-// Wait for a keystroke.  
-waitKey(0); 
- 
-// Destroys all the windows created                        
-destroyAllWindows();
+int main(int argc, char const *argv[])
+{
+    // Check for valid input
+    if (argc != 2)
+    {
+        std::cout << "Invalid input, please try again\n";
+        return 0;
+    }
 
-// Write the image in the same directory
-imwrite("grayscale.jpg", img_grayscale);
+    // Get the name of the image the user is requesting
+    std::string usr_arg = argv[1];
+
+    // Namespace nullifies the use of cv::function();
+    using namespace std;
+    using namespace cv;
+
+    // Check to make sure the file exsists
+    ifstream ifile;
+    ifile.open(usr_arg);
+    if (ifile)
+    {
+        // Read the image
+        Mat usr_img = imread(usr_arg, IMREAD_UNCHANGED);
+
+        // Display the image
+        imshow(usr_arg, usr_img);
+
+        // Wait for a keystroke
+        waitKey(0);
+
+        // Destroy the window created
+        destroyAllWindows();
+    }
+    else
+    {
+        std::cout << "The specified file does not exist";
+    }
+}
