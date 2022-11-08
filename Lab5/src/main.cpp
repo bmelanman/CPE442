f639b2b559b7d8ccd921ad6efdd39108439b4f36
Bryce Melander <brycemelander@gmail.com>
2022-11-08 08:41:46 -0800

    updated

--- Lab5/src/main.cpp
+++ Lab5/src/main.cpp
@@ -3,7 +3,9 @@
 *
 * Description: This program takes full color videos,
 *              converts them to grayscale and applies
-*              the sobel operator frame by frame.
+*              the sobel operator frame by frame. The
+*              speed of the video is dependant on the
+*              time it takes to process each frame.
 *
 * Author: Bryce Melander
 * Co-Authors: Blase Parker, Johnathan Espiritu
@@ -29,6 +31,7 @@ using namespace cv;
 #define G_CONST 0.2126
 #define B_CONST 0.7152
 #define R_CONST 0.0722
+#define NUM_THREADS 4
 
 /***** Structures *****/
 struct thread_data {
@@ -36,39 +39,43 @@ struct thread_data {
     Mat *output{};
     int start{};
     int stop{};
-    int remainder{};
+    int remainder{0};
 };
-
 typedef struct int16x8x8_t {
     int16x8_t val[8];
 } int16x8x8_t;
-
 typedef struct uint8x8x8_t {
     uint8x8_t val[8];
 } uint8x8x8_t;
 
-#define NUM_THREADS 4
 /***** Global Variables *****/
 pthread_barrier_t gray_barrier;
 pthread_barrier_t sobl_barrier;
 
-bool done_flag = false;
+// While-loop lock variable for keeping the threads in their respective filters
+bool thread_lock = false;
+
+// These values will be divided by 256 by the grayscale filter to avoid float math
 const uint8x8_t Green_vect = vdup_n_u8(G_CONST * 256);
 const uint8x8_t Blue_vect = vdup_n_u8(B_CONST * 256);
 const uint8x8_t Red_vect = vdup_n_u8(R_CONST * 256);
+
+// Overflow check used to make sure vectors are not larger
+// than 255 before converting to an 8 bit vector
 const uint16x8_t Overflow_check = vdupq_n_u16(255);
 
+// Kernels used for grayscale filtering
+// Small kernel is used for single pixel processing
 const int16x8_t Gx_kernel_small = {-1, 0, 1, -2, 2, -1, 0, 1};
 const int16x8_t Gy_kernel_small = {1, 2, 1, 0, 0, -1, -2, -1};
 
+// Large kernel is used for processing 8 pixels at a time
 const int16x8x8_t Gx_kernel = {
         {vdupq_n_s16(1), vdupq_n_s16(2), vdupq_n_s16(1), vdupq_n_s16(0),
-         vdupq_n_s16(0), vdupq_n_s16(-1), vdupq_n_s16(-2), vdupq_n_s16(-1)}
-};
+         vdupq_n_s16(0), vdupq_n_s16(-1), vdupq_n_s16(-2), vdupq_n_s16(-1)}};
 const int16x8x8_t Gy_kernel = {
         {vdupq_n_s16(-1), vdupq_n_s16(0), vdupq_n_s16(1), vdupq_n_s16(-2),
-         vdupq_n_s16(2), vdupq_n_s16(-1), vdupq_n_s16(0), vdupq_n_s16(1)}
-};
+         vdupq_n_s16(2), vdupq_n_s16(-1), vdupq_n_s16(0), vdupq_n_s16(1)}};
 
 /***** Prototypes *****/
 void *gray_thread_vector(void *threadArgs);
@@ -79,15 +86,15 @@ void *sobl_thread_vector(void *threadArgs);
 int main(int argc, char const *argv[]) {
 
     // validate input args
-    if (argc != 2 && argc != 3) {
+    if (argc != 2) {
         cout << "Invalid input" << endl;
         exit(-1);
     }
 
     // get video file name
-    string usr_arg = argv[1];
+    string usrVideo_location = argv[1];
     ifstream ifile;
-    ifile.open(usr_arg);
+    ifile.open(usrVideo_location);
 
     // check file exists
     if (!ifile) {
@@ -95,34 +102,35 @@ int main(int argc, char const *argv[]) {
         exit(-1);
     }
 
-    // calculate and display video length and resolution
-    VideoCapture usr_vid(usr_arg);
-    int fps = (int) usr_vid.get(CAP_PROP_FPS);
-    int frame_count = int(usr_vid.get(CAP_PROP_FRAME_COUNT));
-    cout << "Video resolution: " << usr_vid.get(CAP_PROP_FRAME_HEIGHT)
-         << "x" << usr_vid.get(CAP_PROP_FRAME_WIDTH) << endl;
-    cout << "Video length in seconds: " << (frame_count / fps) << "." << ((frame_count / fps) % 1) << endl;
-    cout << "Number of threads: " << NUM_THREADS << endl;
+    // Open the file as a video and display info for user
+    VideoCapture usrVideo(usrVideo_location);
+    auto fps = (uint8_t) usrVideo.get(CAP_PROP_FPS);
+    int num_frames = (int) usrVideo.get(CAP_PROP_FRAME_COUNT);
+    printf("Video Resolution: %dx%d\n", (int) usrVideo.get(CAP_PROP_FRAME_HEIGHT),
+           (int) usrVideo.get(CAP_PROP_FRAME_WIDTH));
+    printf("Video Length: %d.%d seconds\n", num_frames / fps, (num_frames / fps) % 1);
+    printf("Number of threads: %d\n", NUM_THREADS);
 
-    // init image Mats
-    int usr_vid_rows = (int) usr_vid.get(4);
-    int usr_vid_cols = (int) usr_vid.get(3);
-    Mat frame(usr_vid_rows, usr_vid_cols, CV_8UC3);
-    Mat gray_frame(usr_vid_rows, usr_vid_cols, CV_8UC1);
-    Mat sobl_frame(usr_vid_rows - 2, usr_vid_cols - 2, CV_8UC1);
+    // Init Mats for each filter
+    int usrVideo_rows = (int) usrVideo.get(4);
+    int usrVideo_cols = (int) usrVideo.get(3);
+    Mat frame(usrVideo_rows, usrVideo_cols, CV_8UC3);
+    Mat gray_frame(usrVideo_rows, usrVideo_cols, CV_8UC1);
+    Mat sobl_frame(usrVideo_rows - 2, usrVideo_cols - 2, CV_8UC1);
 
-    // init pthreads and barriers
+    // Init pthreads and barriers
     pthread_t gray_threads[NUM_THREADS];
     pthread_t sobl_threads[NUM_THREADS];
     pthread_barrier_init(&gray_barrier, nullptr, NUM_THREADS + 1);
     pthread_barrier_init(&sobl_barrier, nullptr, NUM_THREADS + 1);
 
-    // init and fill thread struct variables
+    // Calculate the number of remaining pixels that can't be grouped in sets of 8
+    int usrVideo_numPixels = usrVideo_rows * usrVideo_cols;
+    int remainder_pixels = usrVideo_numPixels % (8 * NUM_THREADS);
+    int num_pixels = usrVideo_numPixels - remainder_pixels;
+
+    // Init thread struct variables
     struct thread_data gray_data[NUM_THREADS], sobl_data[NUM_THREADS];
-    int img_size = usr_vid_rows * usr_vid_cols;
-    int remainder_pixels = img_size % (8 * NUM_THREADS);
-    int num_pixels = img_size - remainder_pixels;
-    int remainder = 0;
 
     for (int i = 0; i < NUM_THREADS; i++) {
 
@@ -131,38 +139,42 @@ int main(int argc, char const *argv[]) {
         gray_data[i].output = &gray_frame;
         gray_data[i].start = i * num_pixels / NUM_THREADS;
         gray_data[i].stop = (i + 1) * num_pixels / NUM_THREADS - 1;
-        gray_data[i].remainder = remainder;
 
-        if (i == NUM_THREADS - 1) {
-            remainder = remainder_pixels;
+        // Give the last thread any remainder pixels
+        if (i == NUM_THREADS - 1 && remainder_pixels != 0) {
+            gray_data[i].remainder = remainder_pixels;
         }
 
-        // start running the grayscale filter threads
+        // Run the grayscale filter threads
         pthread_create(&gray_threads[i], nullptr, &gray_thread_vector, (void *) &gray_data[i]);
 
         // init the sobel filter thread variables
         sobl_data[i].input = &gray_frame;
         sobl_data[i].output = &sobl_frame;
-        gray_data[i].start = i * usr_vid_rows / NUM_THREADS;
+        sobl_data[i].start = i * usrVideo_rows / NUM_THREADS;
 
-        // special case for the last thread, it must go all the way to the end
-        if (i == NUM_THREADS - 1) { sobl_data[i].stop = usr_vid_rows - 2; }
-        else { gray_data[i].stop = (i + 1) * usr_vid_rows / NUM_THREADS; }
+        // Special case for the last thread, it must go all the way to the end
+        if (i == NUM_THREADS - 1) { sobl_data[i].stop = usrVideo_rows - 2; }
+        else { sobl_data[i].stop = (i + 1) * usrVideo_rows / NUM_THREADS; }
 
-        // start running the sobel filter threads
+        // Run the sobel filter threads
         pthread_create(&sobl_threads[i], nullptr, sobl_thread_vector, (void *) &sobl_data[i]);
     }
 
+    // Allow the grayscale filter to process one frame before the sobel filter runs
+    usrVideo >> frame;
+    pthread_barrier_wait(&gray_barrier);
+
     // Loop through the image file
-    while (usr_vid.isOpened()) {
+    while (usrVideo.isOpened()) {
 
         // Get a frame from the video
-        usr_vid >> frame;
+        usrVideo >> frame;
 
         // If we're all out of frames, the video is over
         if (frame.empty()) {
             // set the done flag high to release the threads from the filters
-            done_flag = true;
+            thread_lock = true;
             break;
         }
 
@@ -171,30 +183,31 @@ int main(int argc, char const *argv[]) {
         pthread_barrier_wait(&sobl_barrier);
 
         // Display the frame
-        imshow(usr_arg, sobl_frame);
+        imshow(usrVideo_location, sobl_frame);
 
         // Hold ESC to exit the video early
-        if ((char) waitKey(2) == 27) {
-            done_flag = true;
+        if ((char) waitKey(1) == 27) {
+            thread_lock = true;
             break;
         }
     }
 
-    // collect all the filters together and rejoin everything with the main thread
+    // Rejoin all threads
     pthread_barrier_wait(&gray_barrier);
     pthread_barrier_wait(&sobl_barrier);
 
+    // Clean up threads
     for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(gray_threads[i], nullptr);
         pthread_join(sobl_threads[i], nullptr);
     }
 
-    // clean up thread barriers
+    // Clean up thread barriers
     pthread_barrier_destroy(&gray_barrier);
     pthread_barrier_destroy(&sobl_barrier);
 
     // clean up video stuff
-    usr_vid.release();
+    usrVideo.release();
     destroyAllWindows();
 
     return 0;
@@ -210,36 +223,49 @@ int main(int argc, char const *argv[]) {
  */
 void *gray_thread_vector(void *threadArgs) {
 
-    auto *thread_data = (struct thread_data *) threadArgs;
-    int remainder = thread_data->remainder;
-    int start = thread_data->start;
-    int stop = thread_data->stop;
-
+    // Function variables
     uchar *origImgData;
     uchar *grayImgData;
     uint8x8x3_t BGR_values;
     uint16x8_t temp;
 
-    while (!done_flag) {
+    // Thread variables
+    auto *thread_data = (struct thread_data *) threadArgs;
+    int remainder = thread_data->remainder;
+    int start = thread_data->start;
+    int stop = thread_data->stop;
+
+    // Lock the threads in the image processing loop until we've processed every frame of the video
+    while (!thread_lock) {
+
+        // Reset data address position for each loop
         origImgData = thread_data->input->data + (start * 3);
         grayImgData = thread_data->output->data + start;
 
+        // Go through the image 8 pixels at a time
         for (int i = start; i < stop; i += 8, origImgData += 24, grayImgData += 8) {
+
+            // Load 8 pixels in a vector
             BGR_values = vld3_u8(origImgData);
 
+            // Multiply each pixel by the predefined grayscale ratios
             temp = vmull_u8(BGR_values.val[0], Blue_vect);
 
             temp = vmlal_u8(temp, BGR_values.val[1], Green_vect);
             temp = vmlal_u8(temp, BGR_values.val[2], Red_vect);
 
+            // Write 8 pixels at a time to grayscale frame
             vst1_u8(grayImgData, vshrn_n_u16(temp, 8));
         }
 
+        // If the end of the image has fewer than 8 pixels, process the remainder one pixel at a time
         for (int i = stop; i < stop + remainder; i++, origImgData += 3, grayImgData++) {
             *grayImgData = (uchar) ((*origImgData * B_CONST) +
                                     (*origImgData * G_CONST) +
                                     (*origImgData * R_CONST));
         }
+
+        // Wait until the main loop moves on to the next image
         pthread_barrier_wait(&gray_barrier);
     }
     pthread_exit(nullptr);
@@ -249,9 +275,15 @@ void *gray_thread_vector(void *threadArgs) {
  * Takes a grayscale image and applies the sobel operator to the given image.
  * @param threadArgs - A struct with variables for the sobel filter
  */
-void *sobl_thread_vector(void *threadArgs){
+void *sobl_thread_vector(void *threadArgs) {
 
-    // init thread variables
+    // Function Variables
+    int i0, i1, i2, G;
+    uint8x8x8_t gray_pixels;
+    int16x8_t Gx_vect, Gy_vect;
+    uint16x8_t G_vect;
+
+    // Thread variables
     auto *thread_data = (struct thread_data *) threadArgs;
     uchar *gray_data = thread_data->input->data;
     uchar *sobl_data = thread_data->output->data;
@@ -263,12 +295,9 @@ void *sobl_thread_vector(void *threadArgs){
     int remainder = (numCols_sobel % 8);
     int col_stop = numCols_sobel - remainder;
 
-    int i0, i1, i2, G;
-    uint8x8x8_t gray_pixels;
-    int16x8_t Gx_vect, Gy_vect;
-    uint16x8_t G_vect;
+    // Lock the threads in the image processing loop until we've processed every frame of the video
+    while (!thread_lock) {
 
-    while (!done_flag) {
         // Loop through the rows and cols of the image and apply the sobel filter
         for (int row = start; row < stop; row++) {
 
@@ -291,6 +320,7 @@ void *sobl_thread_vector(void *threadArgs){
                 gray_pixels.val[6] = vld1_u8(gray_data + i2 + 1 + col);
                 gray_pixels.val[7] = vld1_u8(gray_data + i2 + 2 + col);
 
+                // Multiply each pixel by the X kernel and sum them all together
                 // This operation also clears old values
                 Gx_vect = vmulq_s16(Gx_kernel.val[0], vmovl_u8(gray_pixels.val[0]));
 
@@ -298,7 +328,7 @@ void *sobl_thread_vector(void *threadArgs){
                     Gx_vect = vmlaq_s16(Gx_vect, Gx_kernel.val[i], vmovl_u8(gray_pixels.val[i]));
                 }
 
-                // This operation also clears old values
+                // Repeat for the Y kernel
                 Gy_vect = vmulq_s16(Gy_kernel.val[0], vmovl_u8(gray_pixels.val[0]));
 
                 for (int i = 1; i < 8; i++) {
@@ -311,12 +341,14 @@ void *sobl_thread_vector(void *threadArgs){
                 // Overflow check
                 G_vect = vminq_u16(G_vect, Overflow_check);
 
-                // Write the pixels to the sobel image
+                // The pixel values are guaranteed to be less than 256, so they can be converted
+                // to uint8_t vectors and written to the sobel image 8 pixels at a time
                 vst1_u8(sobl_data + (numCols_sobel * row) + col, vmovn_u16(G_vect));
             }
 
             for (int r_col = col_stop; r_col < numCols_sobel; r_col++) {
 
+                // If there's some remaining pixels, filter them one by one
                 // Reusing variables here
                 G_vect = (uint16x8_t) {
                         gray_data[i0 + r_col], gray_data[i0 + 1 + r_col], gray_data[i0 + 2 + r_col],
@@ -335,6 +367,7 @@ void *sobl_thread_vector(void *threadArgs){
                 sobl_data[numCols_sobel * row + r_col] = (uchar) G;
             }
         }
+
         // Wait until the main loop moves on to the next image
         pthread_barrier_wait(&sobl_barrier);
     }
