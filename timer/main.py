1a6e6b821a85035c0b95fe66368d6e7ba876e29d
Bryce Melander <brycemelander@gmail.com>
2022-12-03 12:46:53 -0800

    updated

--- timer/main.py
+++ timer/main.py
@@ -2,38 +2,39 @@ import re
 import subprocess
 import sys
 
-import ffmpeg
+import sudo
 import keyboard
 
 
 def timer(main_dir, media_dir, num_tests):
-    num_frames = int(ffmpeg.probe(media_dir)['streams'][0]['nb_frames'])
-
     real_time = []
     user_time = []
     sys_time = []
+    num_frames = 0
 
-    for i in range(num_tests):
-        loop_continue = False
+    for _ in range(num_tests):
+        # loop_continue = False
 
         # Run the code to be tested
-        process = subprocess.Popen("time -p " + main_dir + "main " + media_dir, shell=True,
-                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen("time -p " + main_dir + "main " + media_dir, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         # While it is running, check for stop signal
         while process.poll() is None:
-            if keyboard.is_pressed(" "):
-                keyboard.press("ESC")
-                process.kill()
-                loop_continue = True
-
-        while keyboard.is_pressed(" "):
             pass
+        #     if keyboard.is_pressed(" "):
+        #         keyboard.press("ESC")
+        #         process.kill()
+        #         loop_continue = True
+        #
+        # while keyboard.is_pressed(" "):
+        #     pass
+        #
+        # if loop_continue:
+        #     continue
 
-        if loop_continue:
-            continue
-
+        print(re.findall(r'\d*[.]\d+', str(process.communicate()[0])))
         output = re.findall(r'\d*[.]\d+', str(process.communicate()[1]))
+        print(output)
 
         if len(output) != 3:
             print("Time output error: " + str(output))
@@ -43,11 +44,11 @@ def timer(main_dir, media_dir, num_tests):
         user_time.append(float(output[1]))
         sys_time.append(float(output[2]))
 
+    if len(real_time) <= 0 or len(user_time) <= 0 or len(sys_time) <= 0:
+        return
+
     print("Number of tests: %d" % num_tests)
     print("")
-
-    if len(real_time) < 1:
-        return
 
     print("Frame rate avg:  %.3f" % (num_frames / (sum(real_time) / num_tests)))
     print("Real time avg:   %.3f" % (sum(real_time) / num_tests))
@@ -67,14 +68,19 @@ def timer(main_dir, media_dir, num_tests):
     print("")
 
 
-if len(sys.argv) != 5:
-    print("Invalid args: " + str(len(sys.argv)))
-    exit(-1)
+def main():
+    if len(sys.argv) != 5:
+        print("Invalid args: " + str(len(sys.argv)))
+        exit(-1)
 
-lab5_dir = sys.argv[1]
-lab6_dir = sys.argv[2]
-video_dir = sys.argv[3]
-tests = int(sys.argv[4])
+    lab5_dir = sys.argv[1]
+    lab6_dir = sys.argv[2]
+    video_dir = sys.argv[3]
+    tests = int(sys.argv[4])
 
-timer(lab5_dir, video_dir, tests)
-timer(lab6_dir, video_dir, tests)
+    timer(lab5_dir, video_dir, tests)
+    timer(lab6_dir, video_dir, tests)
+
+
+if __name__ == '__main__':
+    main()
