6b4b9a8331ac4f62383a4f83f0f28e05da7a3560
Bryce Melander <brycemelander@gmail.com>
2022-12-04 13:12:47 -0800

    Update main.py

--- timer/main.py
+++ timer/main.py
@@ -105,7 +105,7 @@ def main():
 
     subprocess.run(["clear"])
     print("%%%%%%%%%% Sobel Filter Data Collection %%%%%%%%%%\n")
-    print("Compiler flags used: ")
+    print("Compiler flags used: -Ofast -march=native")
 
     timer(lab5_dir, video_dir, tests, max_time)
     timer(lab6_dir, video_dir, tests, max_time)
